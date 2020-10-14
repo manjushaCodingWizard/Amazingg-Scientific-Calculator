b216363e4b9e26b9b59489eb3cb758f81d59624b
Manjusha Mali <64725125+manjushaCodingWizard@users.noreply.github.com>
2020-10-14 23:43:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+const op = document.getElementById('output-value');
+
+function myclick(val){
+  op.value = op.value+val;
+}
+
+function output(){
+  op.value = eval(op.value);
+}
+
+function allClear(){
+  op.value = "";
+}
+
+function back(){
+  let backVal = op.value;
+  op.value = backVal.substr(0,backVal.length-1);
+}
+
+function percent(){
+  op.value = op.value/100;
+}
+
+function funcSin(){
+  op.value = Math.sin(op.value*Math.PI/180);
+}
+
+function funcCos(){
+  op.value = Math.cos(op.value*Math.PI/180);
+}
+
+function funcTan(){
+  op.value = Math.tan(op.value*Math.PI/180);
+}
+
+function sq(){
+ op.value = Math.pow(op.value,2); 
+}
+
+function funcCube(){
+  op.value = Math.pow(op.value,3); 
+ }
+
+ function funcRoot(){
+  op.value = Math.pow(op.value,1/2); 
+ }
+
+
+
